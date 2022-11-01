540a61888aea3f4b3baf0301bb50e28a93695a17
unknown <kostsiakimstar0109@gmail.com>
2022-11-02 09:40:20 +1000

    Add existing work to Git

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 
 import math
 
-#Определить размеры помещения (объем V, общую площадь ограничивающих поверхностей S)
+#Определить размеры помещения (объем V, общую площадь ограничивающих поверхностей S)
 a = 25
 b = 15
 c = 7
@@ -55,15 +55,54 @@ for i in [0,1,2,3,4,5]: absorption_total_fund.append(absorption_of_wall[i] + abs
 #доп фонд поглощения
 
 #поглощения 20 стульев
-alpha_chair = []
+alpha_chair = [0.15, 0.2, 0.2, 0.25, 0.3, 0.3]
+surface_chair = 0.4*0.4*4*20
+absorption_of_chair = [i * surface_chair for i in alpha_chair]
+
 #поглощения 1 кресла
-alpha_sofa = []
+alpha_sofa = [0.1, 0.12, 0.17, 0.17, 0.12, 0.1]
+surface_sofa = 0.5*0.5*4
+absorption_of_sofa = [i * surface_sofa for i in alpha_sofa]
+
 #поглощения 2 стола
-alpha_table = []
-#surface_table =
+alpha_table = [0.03, 0.02, 0.05, 0.04, 0.04, 0.04]
+surface_table = (2*1+15*2)*2
+absorption_of_table = [i * surface_table for i in alpha_table]
+
 #поглощения 31 человека
 alpha_man = [0.14, 0.22, 0.34, 0.45, 0.43, 0.4]
-surface_man = 1.9
+surface_man = 1.9 * 31
 absorption_of_man = [i * surface_man for i in alpha_man]
 
-print(absorption_of_man)
+#доп фонд поглощения
+absorption_gon_fund = []
+for i in [0,1,2,3,4,5]: absorption_gon_fund.append(absorption_of_chair[i] + absorption_of_sofa[i] + absorption_of_table[i] + absorption_of_man[i])
+
+#добавочный фонд поглощения
+alpha_dobav = [0.075, 0.06, 0.03, 0.03, 0.03, 0.02]
+absorption_dobav_fund = [i* surface_room for i in alpha_dobav]
+
+#result fund
+absorption_res_fund = []
+for i in [0,1,2,3,4,5]: absorption_res_fund.append(absorption_total_fund[i] + absorption_gon_fund[i] + absorption_dobav_fund[i])
+
+#расчетное время реверберации
+alpha_average_calculated = [i / surface_room for i in absorption_res_fund]
+alpha_calculated = [0 - math.log(1-i) for i in alpha_average_calculated]
+T_calculated = [0.161*volume_room/(surface_room*i) for i in alpha_calculated]
+#T_calculated отличаются от T_opt более чем на 10%
+print("расчетное время реверберации отличается от Топт более чем на 10%!")
+
+#additional fund
+absorption_add_fund = []
+for i in [0,1,2,3,4,5]: absorption_add_fund.append(absorption_required[i] - absorption_res_fund[i])
+
+
+
+print(absorption_required)
+print(absorption_total_fund)
+print(absorption_gon_fund)
+print(absorption_dobav_fund)
+print(absorption_res_fund)
+print(absorption_add_fund)
+print(T_calculated)
